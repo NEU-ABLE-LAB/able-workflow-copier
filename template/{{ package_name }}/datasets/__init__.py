08c658aa9feb893dd375f8e51bf855f95b3c0dd8
thisismikekane <mi.kane@northeastern.edu>
2025-06-28 13:38:04 -0400

    Added docstring to {{ package_name }}/{{ module_type }}/__init__.py files

---
+++ template/{{ package_name }}/datasets/__init__.py
@@ -0,0 +1,5 @@
+"""
+Each module (i.e., directory) within the datasets module
+(i.e., this directory) represents a distinct dataset processed
+by the ETL processes within this module.
+"""
---
+++ template/{{ package_name }}/features/__init__.py
@@ -0,0 +1,5 @@
+"""
+Each module (i.e., directory) within the features module
+(i.e., this directory) represents a distinct feature set
+processed by the ETL processes within this module.
+"""
