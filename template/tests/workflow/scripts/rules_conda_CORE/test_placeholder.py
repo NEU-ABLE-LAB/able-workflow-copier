52cffd242cddc55d0ac747472a7e86004b942a3d
thisismikekane <mi.kane@northeastern.edu>
2025-06-29 20:42:55 -0400

    Added dummy test to CORE dir

---
+++ template/tests/workflow/scripts/rules_conda_CORE/test_placeholder.py
@@ -0,0 +1,9 @@
+"""
+Placeholder so pytest doesn't exit with code 5 when the folder is still empty.
+
+Delete or replace this file as soon as you add real tests.
+"""
+
+import pytest
+
+pytest.skip("No tests for this ruleset yet", allow_module_level=True)
